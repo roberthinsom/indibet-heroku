17cf49644fd498f282ba041b99b78370841b043f
Kristine Krem <122358059+roberthinsom@users.noreply.github.com>
2024-06-06 01:41:17 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta name="google-site-verification" content="DhuSUnXQj4FCCfuPdbqQpeIxufJQ8zOizHDrTt5DRAQ" />
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>From Beginner to Pro: Mastering INDIBET's Sports Betting and Casino Games</title>
